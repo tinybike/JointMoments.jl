6dae97683db19719d73f71677213756d7242a599
Jack Peterson <jack@tinybike.net>
2015-02-27 15:38:31 -0800

    densification, matching indices utility functions

--- src/tensors.jl
+++ src/tensors.jl
@@ -1,3 +1,83 @@
+# Swap dimensions of a tensor (ndarray-transpose)
+function _transpose{T<:Real}(tensor::Array{T,3}, permute::Vector{Int})
+    shape = size(tensor)
+    ttensor = zeros(shape)
+    idx = zeros(3)
+    for idx[1] = 1:shape[1]
+        for idx[2] = 1:shape[2]
+            for idx[3] = 1:shape[3]
+                ttensor[idx[permute]...] = tensor[idx...]
+            end
+        end
+    end
+    ttensor
+end
+
+# Swap dimensions of a tensor (ndarray-transpose)
+function _transpose{T<:Real}(tensor::Array{T,4}, permute::Vector{Int})
+    shape = size(tensor)
+    ttensor = zeros(shape)
+    idx = zeros(4)
+    for idx[1] = 1:shape[1]
+        for idx[2] = 1:shape[2]
+            for idx[3] = 1:shape[3]
+                for idx[4] = 1:shape[4]
+                    ttensor[idx[permute]...] = tensor[idx...]
+                end
+            end
+        end
+    end
+    ttensor
+end
+
+function _corners{T<:Real}(tensor::Array{T})
+    shape = size(tensor)
+    N = ndims(tensor)
+    z = zeros(N)
+    corners = zeros(shape...)
+    for i = 1:shape[1]
+        idx = z + i
+        corners[idx...] = tensor[idx...]
+    end
+    corners
+end
+
+function _pairs{T<:Real}(tensor::Array{T}, swapped::Vector{Int})
+    shape = size(tensor)
+    N = ndims(tensor)
+    pairs = zeros(shape)
+    swapped = sort(swapped)
+    for i = 1:shape[1]
+        if swapped == [1,2]
+            pairs[:,:,i] = _corners(convert(Array, slice(tensor, :, :, i)))
+        elseif swapped == [1,3]
+            pairs[:,i,:] = _corners(convert(Array, slice(tensor, :, i, :)))
+        elseif swapped == [2,3]
+            pairs[i,:,:] = _corners(convert(Array, slice(tensor, i, :, :)))
+        end
+    end
+    pairs
+    # X = [i j k l]
+    # counts = {
+    #     repr(i) => 0,
+    # }
+    # for m = 1:4
+    #     idx = repr(X[m])
+    #     counts[idx] = (idx in keys(counts)) ? counts[idx] + 1 : 1
+    # end
+    # D = prod(map(factorial, values(counts)))
+    # factorial(4) / D
+end
+
+function _dense{T<:Real}(tensor::Array{T})
+    refilled = similar(tensor)
+    permute = [1:ndims(tensor)]
+    for p in permutations(permute)
+        refilled += _transpose(tensor, p) - _pairs(tensor, find(p.!=permute))
+    end
+    refilled
+end
+
 # Covariance matrix (for testing)
 function _cov{T<:Real}(data::Matrix{T}; bias::Int=0, dense::Bool=true)
     num_samples, num_signals = size(data)
@@ -90,6 +170,8 @@ function coskew{T<:Real}(data::Matrix{T};
             end
 
         # Triangular: duplicate values ignored
+        # To convert to dense form:
+        #   tensor + 
         else
             @simd for i = 1:num_signals
                 @simd for j = 1:i
